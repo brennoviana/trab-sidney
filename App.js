44bb09119a77d3f3cc4d09c8e5e9c32e5f75a28b
brennovianacoelho <brenno.viana@smartnx.com>
2025-06-20 09:43:04 -0300

    teste

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import {useState} from 'react';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
 
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-
 import Mensagens from './components/Mensagens';
 import Login from './components/Login';
 import Logo from './components/Logo';
